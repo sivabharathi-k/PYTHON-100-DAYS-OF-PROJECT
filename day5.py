7a6a82e6f3b9ec0606c4f20e811ab13486d9e162
Sivabharathi k <sivabharathi.k2024it@sece.ac.in>
2025-03-03 22:06:27 +0530

    Built a customizable password generator with randomization

---
+++ day5.py
@@ -0,0 +1,28 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
+
+print("Welcome to the password generator!")
+pletters = int(input("How many letters would you like in your password?\n"))
+pnumbers = int(input("How many numbers would you like in your password?\n"))
+psymbols = int(input("How many symbols would you like in your password?\n"))
+
+password = []
+
+for i in range(pletters):
+    password.append(random.choice(letters))
+
+for i in range(pnumbers):
+    password.append(str(random.choice(numbers)))  # Fixed this line
+
+for i in range(psymbols):
+    password.append(random.choice(symbols))
+
+random.shuffle(password)
+
+ps = "".join(password) 
+
+print(f"The password is: {ps}")
